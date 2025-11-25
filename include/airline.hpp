24affa08a1886bf3a9110e3c41e0494c3939bc50
Azlan21 <azlanqamar51@gmail.com>
2025-11-25 15:54:41 -0700

    Created declaration and implementation files for objects; completed Airline class declaration

---
+++ include/airline.hpp
@@ -0,0 +1,26 @@
+#ifndef AIRLINE_HPP
+#define AIRLINE_HPP
+
+#include "flight.hpp"
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
+
+class airline
+{
+    private:
+    vector<Flight> flights;
+    string name;
+
+    public:
+    airline();
+    void add_flight(const Flight& f);
+
+    // Getters
+    const vector<Flight>& get_flights() const;
+    const string& get_name() const;
+
+};
+
+#endif
---
+++ include/route.hpp
@@ -0,0 +1,14 @@
+#ifndef ROUTE_HPP
+#define ROUTE_HPP
+
+
+class route
+{
+private:
+
+public:
+    
+};
+
+
+#endif
