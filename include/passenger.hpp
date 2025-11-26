d23fcaeeed85d4dc7711407d688e78d2f8e76c5e
basema-k <basema.khan1@ucalgary.ca>
2025-11-26 02:55:04 -0700

    Update passenger.hpp
    Basic header file for the 'passenger' class

---
+++ include/passenger.hpp
@@ -0,0 +1,45 @@
+#ifndef PASSENGERS
+#define PASSENGERS
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Passenger {
+private:
+    string flight_id;      
+    string first_name;
+    string last_name;
+    string phone_number;
+    string seat;           
+    int id_number;         
+
+public:
+    // constructors
+    Passenger();
+    Passenger(const string& flight,
+              const string& fn,
+              const string& ln,
+              const string& phone,
+              const string& seat_value,
+              int id);
+
+    // setters
+    void setFlightID(const string& id);
+    void setFirstName(const string& fn);
+    void setLastName(const string& ln);
+    void setPhoneNumber(const string& phone);
+    void setSeat(const string& seat_value);
+    void setIDNumber(int id);
+
+    // getters
+    string getFlightID() const;
+    string getFirstName() const;
+    string getLastName() const;
+    string getPhoneNumber() const;
+    string getSeat() const;
+    int getIDNumber() const;
+
+};
+
+#endif
