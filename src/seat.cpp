7ab84ee248a083488b542e63e185bb0abdabb398
basema-k <basema.khan1@ucalgary.ca>
2025-11-26 11:50:11 -0700

    Update seat.cpp

---
+++ src/seat.cpp
@@ -0,0 +1,36 @@
+#include <string>
+using namespace std;
+
+include "Seat.h"
+
+Seat::Seat(int r, char l, char s): row(r), letter(l), status(s)
+{
+}
+int Seat::getRow()
+{
+	return row;
+}
+char Seat::getLetter()
+{
+	return letter;
+
+char Seat::getSeatStatus()
+{
+	return status;
+}
+void Seat::setRow(int r)
+{
+	row = r;
+}
+void Seat::setLetter(char l)
+{
+	letter = l;
+}
+void Seat::setSeatStatus(char s)
+{
+	status = s;
+}
+
+
+
+
