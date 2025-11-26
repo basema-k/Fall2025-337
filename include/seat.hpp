925e2fb6fabdf04134af79440e5dbea79f7b7084
basema-k <basema.khan1@ucalgary.ca>
2025-11-26 02:57:39 -0700

    Update seat.hpp
    Header file for the class 'Seat'

---
+++ include/seat.hpp
@@ -0,0 +1,30 @@
+#ifndef SEAT
+#define SEAT
+
+#include <string>
+using namespace std;
+
+class Seat {
+private:
+    int row;
+    char letter;
+
+public:
+    // constructors
+    Seat();
+    Seat(int r, char l);
+    Seat(const string& seatStr);   
+
+    // getters
+    int getRow() const;
+    char getLetter() const;
+    string getSeatString() const;
+
+    // setters
+    void setRow(int r);
+    void setLetter(char l);
+    void setSeat(const string& seatStr);
+
+};
+
+#endif
