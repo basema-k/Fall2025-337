bf9a4718c6bce9fd765d90f7f79c9fa59319df33
basema-k <basema.khan1@ucalgary.ca>
2025-11-28 03:25:19 -0700

    Update seat.hpp

--- include/seat.hpp
+++ include/seat.hpp
@@ -7,27 +7,26 @@ using namespace std;
 class Seat {
 private:
     int row;
-    char letter;
-    char status;
+    char letter;    
+    char status;    // F : free, O : occupied
 
 public:
-    // constructors
-    Seat();
-    Seat(int r, char l, char s);   
+    // default constructor
+    Seat() : row(0), letter('A'), status('F') {}
+
+    // general constructor 
+    Seat(int r, char l, char s = 'F')
+        : row(r), letter(l), status(s) {}
 
     // getters
-    int getRow() const;
-    char getLetter() const;
-    string getSeatStatus() const;
+    int getRow() const {return row;}
+    char getLetter() const {return letter;}
+    char getStatus() const {return status;}
 
     // setters
-    void setRow(int r);
-    void setLetter(char l);
-    void setSeatStatus(const string& seatStr);
-
+    void setRow(int r) {row = r;}
+    void setLetter(char l) {letter = l;}
+    void setStatus(char s) {status = s;}
 };
 
 #endif
-
-
-
