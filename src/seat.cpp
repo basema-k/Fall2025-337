bb639c7450c2e784871edbea70259c0d4679ad00
basema-k <basema.khan1@ucalgary.ca>
2025-11-28 03:35:40 -0700

    Update seat.cpp

--- src/seat.cpp
+++ src/seat.cpp
@@ -1,37 +1,41 @@
+#include "../include/Seat.hpp"
 #include <string>
 using namespace std;
 
-include "Seat.hpp"
-
-Seat::Seat(int r, char l, char s): row(r), letter(l), status(s)
+// default constructor
+Seat::Seat()
+    : row(0), letter('A'), status('F')   // F : Free seat
 {
 }
-int Seat::getRow()
-{
-	return row;
-}
-char Seat::getLetter()
-{
-	return letter;
 
-char Seat::getSeatStatus()
+// general constructor
+Seat::Seat(int r, char l, char s)
+    : row(r), letter(l), status(s)
 {
-	return status;
-}
-void Seat::setRow(int r)
-{
-	row = r;
-}
-void Seat::setLetter(char l)
-{
-	letter = l;
-}
-void Seat::setSeatStatus(char s)
-{
-	status = s;
 }
 
+// getters
+int Seat::getRow() const {
+    return row;
+}
 
+char Seat::getLetter() const {
+    return letter;
+}
 
+char Seat::getSeatStatus() const {
+    return status;
+}
 
+// setters
+void Seat::setRow(int r) {
+    row = r;
+}
 
+void Seat::setLetter(char l) {
+    letter = l;
+}
+
+void Seat::setSeatStatus(char s) {
+    status = s;
+}
